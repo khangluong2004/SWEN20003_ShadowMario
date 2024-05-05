ae024502406d3b62d2e3dbbb4ee2cc9426f72acf
khangluong2004 <khangluong2004@gmail.com>
2024-05-05 21:22:23 +1000

    Implement Player

--- src/GameEntities/Characters/Player.java
+++ src/GameEntities/Characters/Player.java
@@ -1,19 +1,34 @@
 package GameEntities.Characters;
 
+import GameEntities.Attackers.FireBall;
+import GameEntities.Characters.PowerUps.PowerUpManager;
+import GameEntities.Characters.PowerUps.TimedPowerUp;
+import GameEntities.CollisionInterface.Collidable;
+import GameEntities.CollisionInterface.RadiusCollidable;
 import GameEntities.GameEntity;
+import GameEntities.Movable;
+
+import GameEntities.Attackers.Enemy;
 import GameEntities.PickUpItems.Coin;
+import GameEntities.PickUpItems.DoubleScorePower;
+import GameEntities.PickUpItems.InvinciblePower;
 import GameEntities.Platforms.Platform;
-import bagel.Font;
+import GameEntities.Flags.EndFlag;
+
+
+import GameProperties.GameProps;
 import bagel.Image;
 import bagel.Window;
 import bagel.util.Point;
+
 import enums.GameStage;
 import enums.MoveDirection;
-import utils.DistanceUtils;
-import utils.Message;
-import utils.PercentageUtils;
+import enums.PowerUpItem;
 
+
+import java.util.ArrayList;
 import java.util.Properties;
+import java.util.Set;
 
 /**
  * GameEntity.Characters.Player class, handling the changing image when the player is moving
@@ -21,20 +36,16 @@ import java.util.Properties;
  * Collision side-effects, as explained in GameEntity.Movable interface, is handled by the different
  * entities that collide with GameEntity.Characters.Player (and not the GameEntity.Characters.Player itself), ensuring Open-Closed Principle (I hope)
  */
-public class Player extends GameEntity implements Movable, Collidable {
+public class Player extends GameEntity implements Movable, RadiusCollidable, Killable, Fireable, ScoreContainer {
     // Images and constants
     private final static int START_JUMPING_SPEED = -20;
-    private final static int END_SPEED = 2;
-    private final static int INIT_JUMPING_SPEED = 0;
-    private final static int INIT_SCORE = 0;
+    private final static int DIE_SPEED = 2;
     private final static int JUMP_ACCELERATION = 1;
-    // Store the windowHeight to check if the player is out of the window when the player lost
-    private final Image PLAYER_LEFT;
-    private final Image PLAYER_RIGHT;
+    private final double RADIUS;
 
     // GameEntity.Characters.Player properties
     private boolean lockJump;
-    private final double RADIUS;
+
     // Vertical velocity
     private int velocity;
     private int score;
@@ -42,41 +53,61 @@ public class Player extends GameEntity implements Movable, Collidable {
 
     private GameStage gameStage;
 
-    // Score and health display
-    // scoreMessage and healthMessage is mutable, so not set to final
-    private final String SCORE_INIT_MESSAGE;
-    private Message scoreMessage;
-    private final String HEALTH_INIT_MESSAGE;
-    private Message healthMessage;
+    // Powerup Manage
+    PowerUpManager powerUpManager;
+    private Set<PowerUpItem> allPowerUpItems;
 
 
-    public Player(Point location, Properties gameProps, Properties messageProps){
-        super(new Image(gameProps.getProperty("gameObjects.player.imageLeft")), location);
-        PLAYER_RIGHT = new Image(gameProps.getProperty("gameObjects.player.imageRight"));
-        PLAYER_LEFT = entityImg;
-        RADIUS = Double.parseDouble(gameProps.getProperty("gameObjects.player.radius"));
 
-        Font scoreFont = new Font(gameProps.getProperty("font"), Integer.parseInt(gameProps.getProperty("score.fontSize")));
-        SCORE_INIT_MESSAGE = messageProps.getProperty("score");
-        scoreMessage = new Message(SCORE_INIT_MESSAGE + this.score, new Point(Double.parseDouble(gameProps.getProperty("score.x")),
-                Double.parseDouble(gameProps.getProperty("score.y"))), scoreFont, false);
+    public Player(Point location){
+        super(new ArrayList<Image>(), 0, location);
+        Properties gameProps = GameProps.getGameProps();
 
-        Font healthFont = new Font(gameProps.getProperty("font"), Integer.parseInt(gameProps.getProperty("playerHealth.fontSize")));
-        HEALTH_INIT_MESSAGE = messageProps.getProperty("health");
-        healthMessage = new Message(HEALTH_INIT_MESSAGE + this.health, new Point(Double.parseDouble(gameProps.getProperty("playerHealth.x")),
-                Double.parseDouble(gameProps.getProperty("playerHealth.y"))), healthFont, false);
+        // Store the right image at 0, left image at 1
+        this.entityImages.add(new Image(gameProps.getProperty("gameObjects.player.imageRight")));
+        this.entityImages.add(new Image(gameProps.getProperty("gameObjects.player.imageLeft")));
+        this.RADIUS = Double.parseDouble(gameProps.getProperty("gameObjects.player.radius"));
 
-        this.resetAttributes(gameProps);
-    }
+        // Add the powerup manager
+        this.powerUpManager = new PowerUpManager();
 
-    public void resetAttributes(Properties gameProps){
-        super.resetAttributes(gameProps);
-        this.velocity = INIT_JUMPING_SPEED;
-        this.setScore(INIT_SCORE);
+        this.velocity = 0;
+        this.score = 0;
         this.setHealth(Double.parseDouble(gameProps.getProperty("gameObjects.player.health")));
         this.lockJump = false;
         this.gameStage = GameStage.PLAYING;
-        entityImg = PLAYER_RIGHT;
+    }
+
+    @Override
+    public void updatePerFrame(){
+        // Update the power ups
+        this.powerUpManager.updatePerFrameAllPowerUp();
+        this.allPowerUpItems = powerUpManager.getPowerUpItems();
+    }
+
+    private void setHealth(double newHealth){
+        this.health = Math.max(0, newHealth);
+        if (this.health <= 0){
+            this.gameStage = GameStage.START_LOSING;
+        }
+    }
+
+    public double getHealth(){
+        return this.health;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public boolean isDead() {
+        return false;
+    }
+
+    @Override
+    public boolean isDamageable() {
+        return allPowerUpItems.contains(PowerUpItem.INVINCIBLE);
     }
 
 
@@ -97,9 +128,9 @@ public class Player extends GameEntity implements Movable, Collidable {
         if (direction == MoveDirection.UP){
             this.jump();
         } else if (direction == MoveDirection.LEFT){
-            entityImg = PLAYER_LEFT;
+            this.currentImageIndex = 1;
         } else if (direction == MoveDirection.RIGHT){
-            entityImg = PLAYER_RIGHT;
+            this.currentImageIndex = 0;
         } else if (direction == MoveDirection.CONTINUE){
             location = new Point(location.x, location.y + velocity);
             if (lockJump){
@@ -119,36 +150,6 @@ public class Player extends GameEntity implements Movable, Collidable {
 
     public void draw(){
         super.draw();
-        scoreMessage.write();
-        healthMessage.write();
-    }
-
-    public double getHealth(){
-        return this.health;
-    }
-
-    private void setHealth(double health){
-        this.health = Math.max(0, health);
-        healthMessage.setMessageContent(HEALTH_INIT_MESSAGE + PercentageUtils.toPercentage(this.health));
-
-        // Check if the player lost
-        // If yes, then change the endingStage and
-        // set velocity to END_SPEED
-        if (this.health <= 0){
-            this.gameStage = GameStage.START_LOSING;
-            this.lockJump = false; // Remove any jumping acceleration when dead
-            this.velocity = END_SPEED;
-        }
-    }
-
-
-    public int getScore() {
-        return score;
-    }
-
-    private void setScore(int score){
-        this.score = score;
-        scoreMessage.setMessageContent(SCORE_INIT_MESSAGE + this.score);
     }
 
     public GameStage getEndingStage(){
@@ -158,51 +159,99 @@ public class Player extends GameEntity implements Movable, Collidable {
 
     @Override
     public void startCollideWith(Collidable entity) {
-        if (entity instanceof Enemy){
-            // Down cast enemy and get the damage value
-            // then change health (if first time collided)
-            Enemy enemy = (Enemy) entity;
-            double damage = enemy.getDamageSize();
-
-            if (!enemy.isInflictedDamage()){
-                this.setHealth(this.health + damage);
-            }
-
-        } else if (entity instanceof Platform){
-            // Stop jumping
-            this.lockJump = false;
-            this.velocity = 0;
-
-            // Downcast the entity and get the Offset, location and height values from the platform
-            Platform platform = (Platform) entity;
-            double offsetPixels = platform.getOffsetPixels();
-            Point platformLocation = platform.getLocation();
-            double platformHeight = platform.getHeight();
-
-            // Calculate the location just above the platform after the collision
-            this.location = new Point(this.location.x, platformLocation.y - platformHeight - offsetPixels);
-        } else if (entity instanceof Coin){
-            // Downcast the coin, and change score if first time collided
-            Coin coin = (Coin) entity;
-            if (!coin.isPlayerCollided()){
-                this.setScore(score + coin.getCoinValue());
-            }
-        } else if (entity instanceof EndFlag){
-            // Set winning stage
-            this.gameStage = GameStage.WINNING;
+        // Check if lost
+        if (this.gameStage != GameStage.PLAYING){
+            return;
         }
+
+        if (entity instanceof Enemy){
+            handleCollisionEntity((Enemy) entity);
+        } else if (entity instanceof Platform){
+            handleCollisionEntity((Platform) entity);
+        } else if (entity instanceof Coin){
+            handleCollisionEntity((Coin) entity);
+        } else if (entity instanceof DoubleScorePower){
+            handleCollisionEntity((DoubleScorePower) entity);
+        } else if (entity instanceof InvinciblePower) {
+            handleCollisionEntity((InvinciblePower) entity);
+        } else if (entity instanceof EndFlag){
+            handleCollisionEntity((EndFlag) entity);
+        } else if (entity instanceof FireBall){
+            handleCollisionEntity((FireBall) entity);
+        }
+    }
+
+    private void handleCollisionEntity(Enemy enemy){
+
+        // Down cast enemy and get the damage value
+        // then change health (if first time collided)
+        double damage = enemy.getDamage(this);
+
+        if (!enemy.isInflictedDamage()){
+            this.setHealth(this.health + damage);
+        }
+    }
+
+    private void handleCollisionEntity(Platform platform){
+        // Stop jumping
+        this.lockJump = false;
+        this.velocity = 0;
+
+        // Downcast the entity and get the Offset, location and height values from the platform
+        double offsetPixels = platform.getOffsetPixels();
+        Point platformLocation = platform.getLocation();
+        double platformHeight = platform.getHeight();
+
+        // Calculate the location just above the platform after the collision
+        // Need this calculation to ensure the player is always above the platform, even if the
+        // high velocity of the player leads to collision detection appears after the player overlaps
+        // with the platform
+        this.location = new Point(this.location.x, platformLocation.y - platformHeight - offsetPixels);
+    }
+
+    private void handleCollisionEntity(Coin coin){
+        // Downcast the coin, and change score if first time collided
+        if (!coin.isPlayerCollided()){
+            this.score += coin.getCoinValue();
+        }
+    }
+
+    private void handleCollisionEntity(DoubleScorePower doubleScore){
+        powerUpManager.addPowerUp(new TimedPowerUp(PowerUpItem.DOUBLE_SCORE, doubleScore.getDoubleScoreLastingFrames()));
+    }
+
+    private void handleCollisionEntity(InvinciblePower invincible){
+        powerUpManager.addPowerUp(new TimedPowerUp(PowerUpItem.INVINCIBLE, invincible.getInvincibleLastingFrames()));
+    }
+
+    private void handleCollisionEntity(EndFlag endFlag){
+        // Set winning stage
+        this.gameStage = GameStage.WINNING;
+    }
+
+    private void handleCollisionEntity(FireBall fireBall){
+        // Check if invincible, else change the health
+        if (allPowerUpItems.contains(PowerUpItem.INVINCIBLE)){
+            return;
+        }
+        this.setHealth(this.health + fireBall.getDamage(this));
     }
 
     @Override
     public void endCollideWith(Collidable entity) {}
 
     @Override
-    public double getRadius() {
+    public double getCollisionRadius(Collidable entity) {
         return this.RADIUS;
     }
 
     @Override
-    public double calcDistanceSquared(Point location2) {
-        return DistanceUtils.calcEuclideanDistanceSquared(this.location, location2);
+    public void fire() {
+        // TODO: Fire after set up the playing scene
+    }
+
+    @Override
+    public void target(GameEntity target) {
+        //TODO: Finish after set up the playing scene
     }
 }
